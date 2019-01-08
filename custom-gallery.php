8aee6c75db82085a2cc0b3166f244c9c2bfad315
Donovan Campbell-Gillies <null>
2019-01-07 18:48:35 -0700

    initialized project

---
+++ custom-gallery.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* @package CustomGalleryPlugin
+ *
+ * Plugin Name: Custom Gallery
+ * Description: A highly customizable and completely free gallery plugin.
+ * Version:     1.0
+ * Author:      Donovan Campbell-Gillies
+ * Author URI:  https://github.com/WordPress/classic-editor/
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: custom-gallery
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation. You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+?>
